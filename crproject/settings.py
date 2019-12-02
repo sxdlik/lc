1e0554c0a9277a7f7b719b8bab3c4279dc402e19
sxd <sxdlik@163.com>
2019-12-02 14:53:01 +0800

    部署前

--- crproject/settings.py
+++ crproject/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'crproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
     }
 }
 
@@ -121,6 +121,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 
 
 # 访问优先顺序
